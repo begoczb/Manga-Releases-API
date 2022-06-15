01845278ba77ee4f1736362405b1c69d763315a8
Adrien Deveaux <adrien.dvx@gmail.com>
2022-06-15 17:26:06 +0200

    mangaSeries & mangaVolumes Routes with combination and error handling

--- routes/mangaVolume.routes.js
+++ routes/mangaVolume.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const { isValidObjectId } = require("mongoose");
 const MangaVolume = require("../models/MangaVolume.model.js");
+const MangaSeries = require("../models/MangaSeries.model.js");
 
 //
 router.get("/", async (req, res, next) => {
@@ -10,25 +12,101 @@ router.get("/", async (req, res, next) => {
     const number = req.query.number;
     const releaseDate = req.query.releaseDate;
     const series = req.query.series;
+    const reverseOrder = req.query.reverseOrder;
 
-    let allMangaVolume = await MangaVolume.find();
+    let mangaVolumeFilter = await MangaVolume.find();
+
+    if (reverseOrder) {
+      mangaVolumeFilter = await MangaVolume.find().sort({ title: -1 });
+    }
 
     if (title) {
-      allMangaVolume = await MangaVolume.find({ title: title });
+      mangaVolumeFilter = await MangaVolume.find({ title: title });
+      if (mangaVolumeFilter.length === 0) {
+        res
+          .status(400)
+          .json({ message: "Please provide a manga volume title" });
+        return;
+      }
     }
     if (ISBN) {
-      allMangaVolume = await MangaVolume.find({ ISBN: ISBN });
+      mangaVolumeFilter = await MangaVolume.find({ ISBN: ISBN });
+      if (mangaVolumeFilter.length === 0) {
+        res
+          .status(400)
+          .json({ message: "Please provide a good ISBN reference" });
+        return;
+      }
     }
     if (number) {
-      allMangaVolume = await MangaVolume.find({ number: number });
+      mangaVolumeFilter = await MangaVolume.find({ number: number });
+      if (reverseOrder) {
+        mangaVolumeFilter = await MangaVolume.find({ number: number }).sort({
+          number: -1,
+        });
+      }
+      if (mangaVolumeFilter.length === 0) {
+        res
+          .status(400)
+          .json({ message: "There is no volume wth this number reference" });
+        return;
+      }
     }
     if (releaseDate) {
-      allMangaVolume = await MangaVolume.find({ releaseDate: releaseDate });
+      mangaVolumeFilter = await MangaVolume.find({ releaseDate: releaseDate });
+      if (reverseOrder) {
+        mangaVolumeFilter = await MangaVolume.find({
+          releaseDate: releaseDate,
+        }).sort({
+          releaseDate: -1,
+        });
+      }
+      if (mangaVolumeFilter.length === 0) {
+        res.status(400).json({ message: "Please provide a correct date" });
+        return;
+      }
     }
     if (series) {
-      allMangaVolume = await MangaVolume.find({series});
+      if (!isValidObjectId(series)) {
+        foundMangaSeries = await MangaSeries.findOne({
+          name: series,
+        });
+        console.log(foundMangaSeries);
+        if (foundMangaSeries) {
+          mangaVolumeFilter = await MangaVolume.find({
+            series: foundMangaSeries._id,
+          });
+          // console.log(mangaVolumeFilter);
+        }
+        if (reverseOrder) {
+          mangaVolumeFilter = await MangaVolume.find({
+            series: foundMangaSeries._id,
+          }).sort({
+            number: -1,
+          });
+        }
+        if (foundMangaSeries.length === 0) {
+          res.status(400).json({ message: "No manga series name found" });
+          return;
+        }
+      }
+      if (isValidObjectId(series)) {
+        mangaVolumeFilter = await MangaVolume.findById(series);
+        if (reverseOrder) {
+          mangaVolumeFilter = await MangaVolume.findById(series).sort({
+            number: -1,
+          });
+        }
+        if (mangaVolumeFilter.length === 0) {
+          res
+            .status(400)
+            .json({ message: "Please provide a correct series Id" });
+          return;
+        }
+      }
     }
-    res.status(200).json(allMangaVolume);
+
+    res.status(200).json(mangaVolumeFilter);
   } catch (err) {
     next(err);
   }
