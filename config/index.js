39363f39bc803069ae504415910ee0ee39a351d4
Adrien Deveaux <adrien.dvx@gmail.com>
2022-07-03 13:12:11 +0200

    fix user get route lack a d to Favorite.find

--- config/index.js
+++ config/index.js
@@ -23,7 +23,10 @@ module.exports = (app) => {
   app.use(
     cors({
       credentials: true,
-      origin: process.env.ORIGIN || "http://localhost:3000",
+      origin:
+        process.env.ORIGIN ||
+        "http://localhost:3000" ||
+        "http://localhost:3001",
     })
   );
 
